e3b465a295853fe43cba4adafd52e8565e930166
asamnt <amitsamant@gmail.com>
2022-09-23 11:08:53 +0530

    form validation completed

--- form-validation/index.js
+++ form-validation/index.js
@@ -17,17 +17,23 @@ function showSuccess(input){
     
 }
 
+
+
 //check email
-function isValidEmail(email){
-    return String(email)
+function checkEmail(input){
+    const isValid = String(input.value.trim())
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     )
+    if(isValid){
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is invalid')
+    }
 }
 
 function checkRequired(inputArr){
-
     inputArr.forEach(input => {        
         if(input.value.trim() === ''){
             console.log(input.id)
@@ -36,8 +42,25 @@ function checkRequired(inputArr){
             showSuccess(input)
         }
     });
-
 }
+
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be atleast ${min} characters`)
+    } else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+
+function checkPasswordMatch(input1, input2){
+    if (input1.value !== input2.value){
+        showError(input2, 'Passwords do not match')
+    }
+}
+
 
 function getFieldName(input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1)
@@ -51,7 +74,14 @@ form.addEventListener('submit', (e) => {
 
 
     checkRequired([username, email, password, password2])
+    checkLength(username, 3 , 15)
+    checkLength(password, 6 , 25)
+    checkEmail(email)
+    checkPasswordMatch(password,password2)
 
+
+    //old way of doing things
+    
     // if(username.value === ''){
     //     showError(username, 'Username is required')
     // } else {
